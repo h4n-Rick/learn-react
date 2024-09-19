79ace10e6afc630901edc42b06d452cc36c54f3f
h4n-Rick <haolen1995@gmail.com>
2024-09-19 22:42:42 +0800

    feat: list demo

---
+++ src/List1.tsx
@@ -0,0 +1,50 @@
+import type { FC } from "react";
+
+const List1: FC = () => {
+	// 列表页
+	const questionList = [
+		{ id: "q1", title: "问卷1", isPublished: false },
+		{ id: "q2", title: "问卷2", isPublished: true },
+		{ id: "q3", title: "问卷3", isPublished: false },
+		{ id: "q4", title: "问卷4", isPublished: true },
+	];
+
+	function edit(id: string) {
+		console.log(`edit ${id}`);
+	}
+
+	return (
+		<div>
+			<h1>问卷列表页</h1>
+			<div>
+				{questionList.map((question) => {
+					const { id, title, isPublished } = question;
+					return (
+						<div
+							key={id}
+							className="mt-3 border border-slate-500 p-3"
+						>
+							<strong>{title}</strong>
+							&nbsp;
+							{isPublished ? (
+								<span style={{ color: "green" }}>已发布</span>
+							) : (
+								<span>未发布</span>
+							)}
+							<button
+								type="button"
+								onClick={() => {
+									edit(id);
+								}}
+							>
+								编辑问卷
+							</button>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
+
+export default List1;
